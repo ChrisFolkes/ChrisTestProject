ec003df5e580a3a0e3084d55b81c8083a37af1a3
ChrisFolkes <cmfolkes@hotmail.com>
2017-03-16 19:52:38 -0400

    Chris Commit

--- ChrisTestProject/Form1.cs
+++ ChrisTestProject/Form1.cs
@@ -19,7 +19,7 @@ namespace ChrisTestProject
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello Git");
+            MessageBox.Show("Hello Git - this is my first change!");
         }
     }
 }
